46c34f577c5c2d6c19db19da945e23db1dfb2539
Nicholas Kimball <31974139+NKimball1@users.noreply.github.com>
2019-10-15 19:40:01 -0500

    Add files via upload

---
+++ path-sumSOL.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        
+        if (root == null) return false; 
+        
+        return traversal(root, sum, 0);
+        
+    }
+    
+    private boolean traversal(TreeNode root, int sum, int count) {
+       
+        if (root.left == null && root.right == null) {
+            if (sum == root.val + count) return true;
+        }
+        
+        if (root.left != null) {
+            if (traversal(root.left, sum, count + root.val) == true) return true;
+        }
+        
+        if (root.right != null){
+            if (traversal(root.right, sum, count + root.val) == true) return true;
+        }
+        
+        return false;
+        
+    }
+}
